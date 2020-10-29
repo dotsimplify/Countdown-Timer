4b90207a7c37a19b36737b5ac3d81478c665cbf7
sarvesh <dubeysarvesh5525@gmail.com>
2020-10-29 12:52:20 +0530

    new

---
+++ index.js
@@ -0,0 +1,29 @@
+var x;
+function start() {
+    if(document.getElementById('input1')!=null){
+        var btn = document.getElementById('button1');
+        btn.disabled = true;
+        let initval = document.getElementById('input1').value
+        initval = parseInt(initval)
+        x = setInterval(() => {
+            document.getElementById('counter').innerHTML = initval
+            initval-=1
+            if(initval < 0){
+                clearInterval(x);
+                document.getElementById("counter").innerHTML = "EXPIRED";
+            }
+            console.log(initval)
+        }, 1000);
+    }
+}
+
+function stop() {
+    let cur_val = parseInt(document.getElementById('counter').innerHTML)
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = cur_val
+}
+
+function reset() {
+    stop()
+    document.getElementById('counter').innerHTML = "Timer Reset"
+}
